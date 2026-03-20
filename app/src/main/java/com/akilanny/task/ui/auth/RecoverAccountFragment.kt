b9ad7331fd5dc27ad2335e2398033850eeea2c52
Akilanny-IFES <salveaquiu@gmail.com>
2026-03-20 16:15:22 -0300

    Terceiro dia

---
+++ app/src/main/java/com/akilanny/task/ui/auth/RecoverAccountFragment.kt
@@ -0,0 +1,26 @@
+package com.akilanny.task.ui.auth
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.akilanny.task.R
+import com.akilanny.task.databinding.FragmentRecoverAccountBinding
+
+class RecoverAccountFragment : Fragment() {
+    private var _binding: FragmentRecoverAccountBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
