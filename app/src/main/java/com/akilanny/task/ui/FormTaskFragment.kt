9b1dda48bfeb3f112d6bcce85d530afc2799620d
Akilanny-IFES <salveaquiu@gmail.com>
2026-03-23 18:19:39 -0300

    Quarto dia

---
+++ app/src/main/java/com/akilanny/task/ui/FormTaskFragment.kt
@@ -0,0 +1,20 @@
+package com.akilanny.task.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.akilanny.task.R
+
+
+class FormTaskFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_form_task, container, false)
+    }
+}
--- app/src/main/java/com/akilanny/task/ui/TodoFragment.kt
+++ app/src/main/java/com/akilanny/task/ui/TodoFragment.kt
@@ -3,6 +3,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.akilanny.task.R
 import com.akilanny.task.databinding.FragmentTodoBinding
 
 class TodoFragment : Fragment() {
@@ -16,6 +18,17 @@ class TodoFragment : Fragment() {
     ): View {
         _binding = FragmentTodoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners(){
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
     }
 
 
